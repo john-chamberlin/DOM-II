13af07cdc886570c3c1b246b701d13372c3d7e39
John Chamberlin <johnchamberlin00@gmail.com>
2020-10-27 16:50:30 -0700

    I'M WORKING ON IT SORRY

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+const navLinks = document.querySelectorAll('.nav-link');
+
+Array.from(navLinks).forEach((link) => {
+    link.addEventListener('mouseover', (event) => {
+        event.target.style.color = 'green';
+    })
+})
