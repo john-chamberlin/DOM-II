1a326a121c6986ca76e2046381a5e3bd2a3e3ca8
John Chamberlin <johnchamberlin00@gmail.com>
2020-10-27 17:40:12 -0700

    6 'features' added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,33 @@
 // Your code goes here
 const navLinks = document.querySelectorAll('.nav-link');
 
+
 Array.from(navLinks).forEach((link) => {
     link.addEventListener('mouseover', (event) => {
         event.target.style.color = 'green';
     })
+})
+
+Array.from(navLinks).forEach((link) => {
+    link.addEventListener('mouseout', (event) => {
+        event.target.style.color = 'black';
+    })
+})
+
+document.addEventListener('dblclick', (event) => {
+    event.target.style.color = 'black'
+    event.preventDefault();
+    
+})
+
+document.addEventListener('keydown', (event) => {
+    event.target.style.color = 'orange';
+})
+
+document.addEventListener('keyup', (event) => {
+    event.target.style.color = 'black';
+})
+
+document.addEventListener('click', (event) => {
+    event.target.style.color = 'green';
 })
